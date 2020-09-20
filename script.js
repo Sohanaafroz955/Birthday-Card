979e2502ae8b537c5bf46f6f95636b98c8f65e97
Sohanaafroz <sohanaafroz955@gmail.com>
2020-09-20 12:51:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "sohanaafroz"&&b=="sohana2020")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
